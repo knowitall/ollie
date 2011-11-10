b615c58835bb00467743d98f9fef40677864918c
Michael Schmitz <schmmd@rv-n11.cs.washington.edu>
2011-11-10 09:28:29 -0800

    Uses the new replaceMatcherAt method (instead of replaceNodeMatcherAtIndex incorrectly).

--- src/main/scala/edu/washington/cs/knowitall/pattern/TreePatternLearner.scala
+++ src/main/scala/edu/washington/cs/knowitall/pattern/TreePatternLearner.scala
@@ -104,29 +104,41 @@ object TreePatternLearner {
     }
   }
 
-  def findPattern(lemmas: Set[String], replacements: Map[String, String], graph: DependencyGraph) = {
+  def findPattern(lemmas: Set[String], 
+    replacements: Map[String, String], 
+    graph: DependencyGraph) = {
+
     // find paths containing lemma
     val bipaths = findBipath(lemmas, graph)
-      // make sure each path contains exactly one of each of the replacement targets
-      .filter(bip => replacements.keys.forall(arg => bip.nodes.count(node => node.text.contains(arg)) == 1))
+      // make sure each path contains exactly one of each 
+      // of the replacement targets
+      .filter(bip => 
+        replacements.keys.forall (key => 
+          bip.nodes.count(node => node.text.contains(key)) == 1))
       
     bipaths.map { bip =>
-        val rep = replacements.map { case (target, replacement) => 
-          (bip.nodes.indexWhere( _.text.contains(target)), new CaptureNodeMatcher(replacement)) }
-        rep.foldRight(new Pattern(bip)) { case(rep, pat) =>
-          pat.replaceNodeMatcherAtIndex(rep._1, rep._2)
-        }
+      // get the indices where we need to make a replacement
+      // and pair them with the replacement itself
+      val rep = replacements.map { case (target, replacement) => 
+        val index = 2*bip.nodes.indexWhere( _.text.contains(target))
+        (index, new CaptureNodeMatcher(replacement)) 
+      }
+
+      // make the replacements
+      rep.foldRight(new Pattern(bip)) { case(rep, pat) =>
+        pat.replaceMatcherAt(rep._1, rep._2)
+      }
     }
   }
 
   def findPatternsForLDA(lemmas: Set[String], replacements: Map[String, String], rel: String, graph: DependencyGraph) = {
-    // arg1 comes first
+    // arg1 comes before other replacements
     val patterns = findPattern(lemmas, replacements, graph)
       .filter(_
         .matchers.find(_
           .isInstanceOf[CaptureNodeMatcher]).map(_
           .asInstanceOf[CaptureNodeMatcher].alias == "arg1").getOrElse(false)).toList
-          
+
     // find the best part to replace with rel
     patterns.map { pattern => 
       val relindex = pattern.matchers.indexWhere (_ match {
@@ -134,7 +146,10 @@ object TreePatternLearner {
         case _ => false
       })
       
-      val p = pattern.replaceNodeMatcherAtIndex(relindex, new CaptureNodeMatcher("rel"))
+      // replace rel
+      val p = pattern.replaceMatcherAt(relindex, new CaptureNodeMatcher("rel"))
+
+      // find all DefaultNodeMatchers.  These are the slots.
       val slots = p.matchers.zipWithIndex flatMap {
         case (nm, index) => nm match {
           case nm: DefaultNodeMatcher => List((nm.label, index))
@@ -143,19 +158,24 @@ object TreePatternLearner {
       }
       
       val (slotLabels, _) = slots.unzip
-      (slots.zipWithIndex.map { 
-        case ((label, index), i) => ("slot" + i, index)
-      }.foldRight(p) { case ((replacement, index), p) =>
-        (p.replaceNodeMatcherAtIndex(index, new CaptureNodeMatcher(replacement)))
-      }, slotLabels)
+
+      val patternWithReplacedSlots = slots.zipWithIndex.map { 
+        case ((label, matcherIndex), slotIndex) => ("slot" + slotIndex, matcherIndex)
+      }.foldRight(p) {
+        case ((replacement, index), p) =>
+          (p.replaceMatcherAt(index, new CaptureNodeMatcher(replacement)))
+      }
+
+      (patternWithReplacedSlots, slotLabels)
     }
   }
 }
 
-object TreePatternConsole {
-import TreePatternLearner._
+object BuildTreePatterns {
+  import TreePatternLearner._
   
   def main(args: Array[String]) {
+    // file with dependencies
     val source =
       if (args.length > 0) Source.fromFile(args(0)).getLines
       else Source.stdin.getLines
@@ -168,8 +188,11 @@ import TreePatternLearner._
       val graph = new DependencyGraph(line, dependencies).collapseNounGroups.collapseNNPOf
       
       val patterns = findPatternsForLDA(lemmas, Map(arg1 -> "arg1", arg2 -> "arg2"), rel, graph)
-      if (patterns.size > 0) {
-        println((rel :: arg1 :: arg2 :: lemmas :: patterns.toList).mkString("\t"))
+      for (pattern <- patterns) {
+        val (pat, slots) = pattern
+        if (slots.length == 0) {
+          println((List(rel, arg1, arg2, lemmas.mkString(" "), pat) ::: slots).mkString("\t"))
+        }
       }
     }
   }
