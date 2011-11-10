bb0854a20f2531449f5c16dd1606a7115a6dda59
Michael Schmitz <michael@schmitztech.com>
2011-11-09 17:10:40 -0800

    Using nlptools instead of parser.

--- scripts/create_lda_input.sh
+++ scripts/create_lda_input.sh
@@ -1,3 +1,3 @@
 # 1 -- lda directory
 cd ..
-mvn -q -e compile exec:java -Dexec.mainClass=edu.washington.cs.knowitall.pattern.lda.CreateLDAFiles -Dexec.args="'$1/raw/train.txt' '$1/ldainput'"
+mvn -q -e -f ../pom.xml compile exec:java -Dexec.mainClass=edu.washington.cs.knowitall.pattern.lda.CreateLDAFiles -Dexec.args="'$1/raw/train.txt' '$1/ldainput'"
