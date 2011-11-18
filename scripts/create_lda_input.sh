b6172a244e6c072ad05ecaf50828d93ed8a21e34
Michael Schmitz <schmmd@cs.washington.edu>
2011-11-17 16:14:27 -0800

    Added code to create a test set.

--- scripts/create_lda_input.sh
+++ scripts/create_lda_input.sh
@@ -1,3 +1,4 @@
 # 1 -- lda directory
 cd ..
-mvn -q -e -f ../pom.xml compile exec:java -Dexec.mainClass=edu.washington.cs.knowitall.pattern.lda.CreateLDAFiles -Dexec.args="'$1/raw/train.txt' '$1/ldainput'"
+mkdir -p "$1/ldainput"
+mvn -q -e -f pom.xml compile exec:java -Dexec.mainClass=edu.washington.cs.knowitall.pattern.lda.CreateLDAFiles -Dexec.args="'$1/raw/train.txt' '$1/ldainput'"
