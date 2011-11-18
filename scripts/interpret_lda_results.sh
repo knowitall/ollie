2e0605cf8a07dc43793258d391a662d2150dd37d
Michael Schmitz <schmmd@cs.washington.edu>
2011-11-17 16:19:24 -0800

    Added script to interpret results.

---
+++ scripts/interpret_lda_results.sh
@@ -0,0 +1,5 @@
+# $1 -- lda folder
+STATS=$1/stats
+mkdir -p $STATS
+cd ..
+mvn -q -e compile exec:java -Dexec.mainClass=edu.washington.cs.knowitall.pattern.lda.Distributions -Dexec.args="'$1' '$STATS'"
