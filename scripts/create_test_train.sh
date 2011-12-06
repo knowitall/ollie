4f151a9e6cea87aefc121c1e16b7bdff5201bdb8
Michael Schmitz <schmmd@cs.washington.edu>
2011-12-05 16:35:26 -0800

    Renamed rows.txt to patterned.txt

--- scripts/create_test_train.sh
+++ scripts/create_test_train.sh
@@ -1,5 +1,5 @@
 # 1 -- lda directory
-ROWS="$1/raw/rows.txt"
+ROWS="$1/raw/patterned.txt"
 TEST="$1/raw/test.txt"
 TRAIN="$1/raw/train.txt"
 mvn -q -e compile exec:java -Dexec.mainClass=edu.washington.cs.knowitall.pattern.lda.CreateTestSet -Dexec.args="$ROWS $TEST $TRAIN"
