6d375068bee4d98b58ce89be201a51b23aa82210
Michael Schmitz <schmmd@cs.washington.edu>
2011-12-12 16:10:35 -0800

    removed println

--- src/test/scala/edu/washington/cs/knowitall/pattern/TreePatternLearnerSpec.scala
+++ src/test/scala/edu/washington/cs/knowitall/pattern/TreePatternLearnerSpec.scala
@@ -20,7 +20,6 @@ object TreePatternLearnerSpec extends Specification {
     val graph = DependencyGraph(deps.map(_.lemmatize(MorphaStemmer.instance))).normalize
     val patterns = TreePatternLearner.findPatternsForLDA(graph, lemmas, Map(arg1 -> "arg1", arg2 -> "arg2"), rel, None)
     patterns.size must_== 1
-    println(patterns.head)
     patterns.head._1.toString must_== "{arg1} <nsubjpass< {rel:VBN} >prep_to> {arg2}"
   }
 }
