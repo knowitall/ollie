f7106e238507bfa9a9b0c2d9b57db92c52aa7707
Michael Schmitz <schmmd@cs.washington.edu>
2011-12-14 11:27:46 -0800

    Added keep_common_patterns

--- scripts/convert_lda_output.sh
+++ scripts/convert_lda_output.sh
@@ -1,2 +1,2 @@
 # 1 -- lda directory
-mvn -q -e compile exec:java -Dexec.mainClass=edu.washington.cs.knowitall.pattern.lda.ConvertLDAOutput
+mvn -q -e compile exec:java -Dexec.mainClass=edu.washington.cs.knowitall.pattern.lda.ConvertLDAOutput -Dexec.args="$1/ldaoutput/output-raw.txt" > "$1/ldaoutput/output.txt"
