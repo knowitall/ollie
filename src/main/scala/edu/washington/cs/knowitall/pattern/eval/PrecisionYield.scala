19f8bbb533c4307b8afd800946c823368f6b2eab
Michael Schmitz <michael@schmitztech.com>
2012-03-08 19:22:36 -0800

    Improved command-line arguments.

--- src/main/scala/edu/washington/cs/knowitall/pattern/eval/PrecisionYield.scala
+++ src/main/scala/edu/washington/cs/knowitall/pattern/eval/PrecisionYield.scala
@@ -5,21 +5,44 @@ import scopt.OptionParser
 import java.io.File
 import scala.io.Source
 import common.stats.Analysis
+import java.io.PrintWriter
 
 object PrecisionYield {
+  abstract class Settings {
+    def scoredFile: File
+    def outputFile: Option[File]
+  }
+  
   def main(args: Array[String]) = {
+    val settings = new Settings {
+      var scoredFile: File = _
+      var outputFile: Option[File] = None 
+    }
+    
     val parser = new OptionParser("precyield") {
       var scoredFile: File = _
 
-      arg("scored", "scored extractions", { path: String => scoredFile = new File(path) })
+      arg("scored", "scored extractions file", { path: String => settings.scoredFile = new File(path) })
+      argOpt("output", "output file", { path: String => settings.outputFile = Some(new File(path)) })
     }
 
     if (parser.parse(args)) {
-      val scores = Score.loadScoredFile(parser.scoredFile).sortBy(_.confidence).reverse
-      val input = scores.map(scored => (scored.confidence, scored.score))
-      
+      run(settings)
+    }
+  }
+  
+  def run(settings: Settings) = {
+    val scores = Score.loadScoredFile(settings.scoredFile).sortBy(_.confidence).reverse
+    val input = scores.map(scored => ("%.4f".format(scored.confidence), scored.score))
+
+    using {
+      settings.outputFile match {
+        case Some(file) => new PrintWriter(file)
+        case None => new PrintWriter(System.out)
+      }
+    } { writer =>
       for ((conf, yld, pr) <- Analysis.precisionYieldMeta(input)) {
-        println(conf + "\t" + yld + "\t" + pr)
+        writer.println(conf + "\t" + yld + "\t" + pr)
       }
     }
   }
