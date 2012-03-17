67e98837541a5df412149ceb099c312f7bd5f372
Michael Schmitz <michael@schmitztech.com>
2012-03-16 17:11:43 -0700

    Updated extractor script for new extractor name.

--- scripts/extractor.sh
+++ scripts/extractor.sh
@@ -1,1 +1,2 @@
-mvn -q -e exec:java -Dexec.mainClass=edu.washington.cs.knowitall.pattern.PatternExtractor -Dexec.args="$*"
+echo "$*"
+mvn -q -e exec:java -Dexec.mainClass=edu.washington.cs.knowitall.pattern.OpenParse -Dexec.args="$*"
