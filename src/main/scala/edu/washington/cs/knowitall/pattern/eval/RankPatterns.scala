d4c9b6d21acf89b04951f776fd039f09303a9cd0
Michael Schmitz <michael@schmitztech.com>
2012-03-20 16:20:31 -0700

    Output yield alongside precision.

--- src/main/scala/edu/washington/cs/knowitall/pattern/eval/RankPatterns.scala
+++ src/main/scala/edu/washington/cs/knowitall/pattern/eval/RankPatterns.scala
@@ -34,7 +34,11 @@ object RankPatterns {
   def run(settings: Settings) = {
     val scores = Score.loadScoredFile(settings.scoredFile).sortBy(_.confidence).reverse
     val grouped = scores.groupBy(_.extra(0))
-      .mapValues(scoreds => scoreds.map(x => if (x.score) 1 else 0).sum.toDouble / scoreds.size.toDouble)
+      .mapValues { scoreds =>
+        val yld = scoreds.map(x => if (x.score) 1 else 0).sum
+        val precision = yld.toDouble / scoreds.size.toDouble
+        (precision, yld)
+      }
 
     using {
       settings.outputFile match {
@@ -42,8 +46,8 @@ object RankPatterns {
         case None => new PrintWriter(System.out)
       }
     } { writer =>
-      for ((pattern, score) <- grouped.toSeq.sortBy(-_._2)) {
-        println(pattern+"\t"+score)
+      for ((pattern, (p, y)) <- grouped.toSeq.sortBy(_._2).reverse) {
+        println(pattern+"\t"+p+"\t"+y)
       }
     }
   }
