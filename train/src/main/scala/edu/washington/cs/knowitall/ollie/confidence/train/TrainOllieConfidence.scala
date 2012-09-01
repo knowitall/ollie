9f87e301e085081400a5753c362ee56a273cca9c
Michael Schmitz <michael@schmitztech.com>
2012-09-01 00:28:50 -0700

    Added code to train a confidence function.

--- train/src/main/scala/edu/washington/cs/knowitall/ollie/confidence/train/TrainOllieConfidence.scala
+++ train/src/main/scala/edu/washington/cs/knowitall/ollie/confidence/train/TrainOllieConfidence.scala
@@ -1,5 +1,55 @@
-package edu.washington.cs.knowitall.ollie.confidence.train
+import scala.io.Source
 
-class TrainOllieConfidenceFunction {
+import breeze.classify._
+import breeze.data.Example
+import breeze.linalg.DenseVector
 
+import scopt.mutable.OptionParser
+
+import edu.washington.cs.knowitall.ollie.OllieExtractionInstance
+import edu.washington.cs.knowitall.ollie.ScoredOllieExtractionInstance
+import edu.washington.cs.knowitall.ollie.confidence.FeatureSet
+import edu.washington.cs.knowitall.ollie.confidence.OllieFeatureSet
+import edu.washington.cs.knowitall.common.Resource.using
+
+class TrainOllieConfidenceFunction(val features: FeatureSet[OllieExtractionInstance]) {
+  def train(instances: Iterable[ScoredOllieExtractionInstance]) = {
+    val examples = instances.zipWithIndex map { case (scored, i) =>
+      val vector = DenseVector(features.vectorize(scored.inst).toArray)
+      Example[Int, DenseVector[Double]](if (scored.score) 1 else 0, vector, id=i.toString)
+    }
+    
+    val classifier = new LogisticClassifier.Trainer[Int,DenseVector[Double]].train(examples)
+  }
+}
+
+object TrainOllieConfidenceFunction {
+   abstract class Settings {
+     def inputFile: File
+   }
+
+  def main(args: Array[String]) = {
+    object settings extends Settings {
+      var inputFile: File = _
+    }
+
+    val parser = new OptionParser("scoreextr") {
+      arg("labelled", "labelled extractions", { path: String => settings.inputFile = new File(path) })
+    }
+
+    if (parser.parse(args)) {
+      run(settings)
+    }
+  }
+  
+  def run(settings: Settings) = {
+    val trainer = new TrainOllieConfidenceFunction(OllieFeatureSet)
+    
+    val data = 
+      using (Source.fromFile(settings.inputFile)) { source =>
+        (source.getLines map (ScoredOllieExtractionInstance.tabDeserialize)).toList
+      }
+    
+    trainer.train(data)
+  }
 }
