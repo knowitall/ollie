176be269e24bd06d0c0286a915ad20d91efbab5b
Michael Schmitz <michael@schmitztech.com>
2012-09-04 15:42:14 -0700

    Trainer spits out feature weights.

--- train/src/main/scala/edu/washington/cs/knowitall/ollie/confidence/train/TrainOllieConfidence.scala
+++ train/src/main/scala/edu/washington/cs/knowitall/ollie/confidence/train/TrainOllieConfidence.scala
@@ -1,34 +1,19 @@
-import scala.io.Source
+package edu.washington.cs.knowitall.ollie.confidence.train
 
+import scala.io.Source
 import breeze.classify._
 import breeze.data.Example
 import breeze.linalg.DenseVector
-
 import scopt.mutable.OptionParser
-
 import edu.washington.cs.knowitall.ollie.OllieExtractionInstance
 import edu.washington.cs.knowitall.ollie.ScoredOllieExtractionInstance
 import edu.washington.cs.knowitall.ollie.confidence.FeatureSet
 import edu.washington.cs.knowitall.ollie.confidence.OllieFeatureSet
 import edu.washington.cs.knowitall.common.Resource.using
+import java.io.File
 
-class TrainOllieConfidenceFunction(val features: FeatureSet[OllieExtractionInstance]) {
-  def train(instances: Iterable[ScoredOllieExtractionInstance]) = {
-    val examples = instances.zipWithIndex map { case (scored, i) =>
-      val vector = DenseVector(features.vectorize(scored.inst).toArray)
-      Example[Int, DenseVector[Double]](if (scored.score) 1 else 0, vector, id=i.toString)
-    }
-    
-    val classifier = new LogisticClassifier.Trainer[Int,DenseVector[Double]].train(examples)
-  }
-}
-
-object TrainOllieConfidenceFunction {
-   abstract class Settings {
-     def inputFile: File
-   }
-
-  def main(args: Array[String]) = {
+object TrainOllieConfidence {
+  def main(args: Array[String]) {
     object settings extends Settings {
       var inputFile: File = _
     }
@@ -41,15 +26,33 @@ object TrainOllieConfidenceFunction {
       run(settings)
     }
   }
-  
+
+   abstract class Settings {
+     def inputFile: File
+   }
+
+
   def run(settings: Settings) = {
-    val trainer = new TrainOllieConfidenceFunction(OllieFeatureSet)
-    
-    val data = 
+    val trainer = new TrainOllieConfidence(OllieFeatureSet)
+
+    val data =
       using (Source.fromFile(settings.inputFile)) { source =>
         (source.getLines map (ScoredOllieExtractionInstance.tabDeserialize)).toList
       }
-    
+
     trainer.train(data)
+  }
+}
+
+class TrainOllieConfidence(val features: FeatureSet[OllieExtractionInstance]) {
+  def train(instances: Iterable[ScoredOllieExtractionInstance]) = {
+    val examples = instances.zipWithIndex map { case (scored, i) =>
+      val vector = DenseVector(features.vectorize(scored.inst).toArray)
+      Example[Boolean, DenseVector[Double]](scored.score, vector, id=i.toString)
+    }
+
+    val classifier = new LogisticClassifier.Trainer[Boolean,DenseVector[Double]].train(examples)
+
+    (features.featureNames.iterator zip classifier.featureWeights.indexed(true).iterator) foreach { case (name, weight) => println(name + ": " + weight) }
   }
 }
