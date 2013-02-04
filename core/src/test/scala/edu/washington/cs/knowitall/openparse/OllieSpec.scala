bf2c9008bb1d1c4d45954a0ab01280e2f0513dbb
Michael Schmitz <michael@schmitztech.com>
2013-02-04 12:39:59 -0800

    Add test case to make sure classifier doesn't bomb.

--- core/src/test/scala/edu/washington/cs/knowitall/openparse/OllieSpec.scala
+++ core/src/test/scala/edu/washington/cs/knowitall/openparse/OllieSpec.scala
@@ -10,12 +10,14 @@ import edu.washington.cs.knowitall.tool.stem.MorphaStemmer
 import edu.washington.cs.knowitall.ollie.Ollie
 import edu.washington.cs.knowitall.ollie.OllieExtractionInstance
 import edu.washington.cs.knowitall.ollie.ScoredOllieExtractionInstance
+import edu.washington.cs.knowitall.ollie.confidence.OllieConfidenceFunction
 
 @RunWith(classOf[JUnitRunner])
 object OllieSpecTest extends Specification {
   val ollie = new Ollie(OpenParse.withDefaultModel())
+  val conf = OllieConfidenceFunction.loadDefaultClassifier()
 
-  "OpenParse finds an example extraction" in {
+  "Ollie finds an example extraction" in {
     val graph = DependencyGraph.deserialize("(._._5_37), nsubj(finds_VBZ_1_10, OpenParse_NNP_0_0); dobj(finds_VBZ_1_10, extraction_NN_4_27); det(extraction_NN_4_27, an_DT_2_16); nn(extraction_NN_4_27, example_NN_3_19)")
     val extrs = ollie.extract(graph)
 
@@ -24,5 +26,11 @@ object OllieSpecTest extends Specification {
 
     val scored = new ScoredOllieExtractionInstance(true, extr)
     scored must_== ScoredOllieExtractionInstance.tabDeserialize(scored.tabSerialize)
+  }
+
+  "Ollie confidence function executes" in {
+    val graph = DependencyGraph.deserialize("(._._5_37), nsubj(finds_VBZ_1_10, OpenParse_NNP_0_0); dobj(finds_VBZ_1_10, extraction_NN_4_27); det(extraction_NN_4_27, an_DT_2_16); nn(extraction_NN_4_27, example_NN_3_19)")
+    val extrs = ollie.extract(graph)
+    extrs map conf must not(throwA[Exception])
   }
 }
